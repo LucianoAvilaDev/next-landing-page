bbb173c839d4e7a588eb3b2a66a676993a0505d1
Luciano Dev <luciano.avila.dev@outlook.com>
2022-10-28 09:01:40 -0300

    contacts responsivity

--- src/components/ContentContacts/ContentContacts.tsx
+++ src/components/ContentContacts/ContentContacts.tsx
@@ -7,26 +7,26 @@ type Props = {
 const ContentContacts = ({ id }: Props) => {
     return (
         <div id={id}>
-            <div className="flex items-center justify-center w-full min-h-[30vw] bg-black/90">
+            <div className="flex flex-col items-center justify-center w-full min-h-[25vw] bg-black/90">
                 <div className="flex flex-col sm:flex-row justify-between border-gray-100 lg:w-2/4 sm:w-3/4 align-center py-6">
                     <div className="flex flex-col px-4 ">
                         <div className="text-center sm:text-justify py-6 text-3xl text-white/80">
                             Landing Page
                         </div>
-                        <div className="sm:text-start text-justify pt-4 text-sm text-white/80">
+                        <div className="sm:text-start text-justify pt-4 text-md text-white/80">
                             Landing Page - um projeto de estudo em React +
                             Nextjs
                         </div>
-                        <div className="sm:text-start text-justify text-sm text-white/80">
+                        <div className="sm:text-start text-justify text-md text-white/80">
                             Desenvolvido por{' '}
                             <span className="font-bold text-lg">
                                 Luciano Ávila
                             </span>
                         </div>
-                        <div className="sm:text-start text-justify text-sm text-white/80">
+                        <div className="sm:text-start text-justify text-md text-white/80">
                             Rua Washington Drumond, nº 165, Centro
                         </div>
-                        <div className="sm:text-start text-justify text-sm text-white/80">
+                        <div className="sm:text-start text-justify text-md text-white/80">
                             Bocaiúva, MG - Brasil
                         </div>
                     </div>
@@ -35,22 +35,22 @@ const ContentContacts = ({ id }: Props) => {
                             Contatos
                         </div>
 
-                        <div className="text-xl pb-2 font-bold text-white/80">
+                        <div className="text-lg font-bold text-white/80">
                             Luciano Thadeu Ávila Diniz
                         </div>
-                        <div className="text-sm text-white/80">
+                        <div className="text-md text-white/80">
                             De segunda a sexta de 08:00 as 18:00.
                         </div>
-                        <div className="text-sm text-white/80">
+                        <div className="text-md text-white/80">
                             Whatsapp: (38) 9 9987 8646
                         </div>
-                        <div className="text-sm text-white/80">
+                        <div className="text-md text-white/80">
                             Email: luciano.avila.dev@outlook.com
                         </div>
-                        <div className="text-xl pt-6  text-white/80">
-                            Será um prazer te atender!
-                        </div>
                     </div>
+                </div>
+                <div className="text-xl pt-4 sm:pt-8  text-white/80">
+                    Será um prazer te atender!
                 </div>
             </div>
         </div>
